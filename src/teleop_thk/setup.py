680a2001ea478a5ff06bbb9b1e1ef5cceb30e85b
Roger22 <joserogelioruiz12@gmail.com>
2025-10-28 19:30:45 +0100

    teleop launch

--- src/teleop_thk/setup.py
+++ src/teleop_thk/setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = 'teleop_thk'
 
@@ -7,15 +9,22 @@ setup(
     version='0.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
+        # required for ROS 2 package index
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+
+        # include launch files
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
+
+        # include rviz config files (optional but recommended)
+        (os.path.join('share', package_name, 'rviz'), glob('rviz/*.rviz')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='roger',
     maintainer_email='joserogelioruiz12@gmail.com',
-    description='TODO: Package description',
+    description='Teleop launch setup with RViz and static TF publisher',
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
